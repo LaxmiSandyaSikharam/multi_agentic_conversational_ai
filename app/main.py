312f5106a08f95fadd9a2fd1fad82707c6c43f29
LaxmiSandyaSikharam <laxmisandya.sikharam05@gmail.com>
2025-07-12 11:41:43 -0400

    env an main

--- app/main.py
+++ app/main.py
@@ -1,24 +1,46 @@
+#from fastapi import FastAPI
+#from fastapi.middleware.cors import CORSMiddleware
+
+#from app.routes import chat, upload, crm
+
+#app = FastAPI(
+#    title="Multi-Agentic Conversational AI System",
+ #   description="LLM + RAG + CRM chatbot",
+#    version="1.0.0"
+#)
+
+# Allow CORS (for testing with Postman or browser)
+#app.add_middleware(
+   # CORSMiddleware,
+  #  allow_origins=["*"],
+   # allow_credentials=True,
+   # allow_methods=["*"],
+   # allow_headers=["*"],
+#)
+
+# Include API routes
+#app.include_router(chat.router, prefix="/chat", tags=["Chat"])
+#app.include_router(upload.router, prefix="/upload_docs", tags=["Document Upload"])
+#app.include_router(crm.router, prefix="/crm", tags=["CRM"])
+
+
+
 from fastapi import FastAPI
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
+import os
 
 from app.routes import chat, upload, crm
 
-app = FastAPI(
-    title="Multi-Agentic Conversational AI System",
-    description="LLM + RAG + CRM chatbot",
-    version="1.0.0"
-)
+app = FastAPI()
 
-# Allow CORS (for testing with Postman or browser)
-app.add_middleware(
-    CORSMiddleware,
-    allow_origins=["*"],
-    allow_credentials=True,
-    allow_methods=["*"],
-    allow_headers=["*"],
-)
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
 
-# Include API routes
+@app.get("/")
+def serve_homepage():
+    return FileResponse(os.path.join("frontend", "index.html"))
+
+# Include all routes
 app.include_router(chat.router, prefix="/chat", tags=["Chat"])
 app.include_router(upload.router, prefix="/upload_docs", tags=["Document Upload"])
 app.include_router(crm.router, prefix="/crm", tags=["CRM"])
